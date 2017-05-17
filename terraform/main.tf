46df61c833e74df121e97b39a5de51728e07701b
dhvogel <dhvogel2468@gmail.com>
2017-05-17 17:45:09 -0400

    bump container versions in prod, added elb

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,9 @@ provider "aws" {
   region = "${var.aws_region}"
 }
 
-resource "aws_security_group" "cfcorp" {
+
+
+resource "aws_security_group" "cfcorp_web_sg" {
   name        = "${var.sg_name}"
   description = "${var.sg_description}"
 
@@ -43,16 +45,50 @@ resource "aws_security_group" "cfcorp" {
   }
 }
 
-resource "aws_instance" "web" {
+
+
+resource "aws_instance" "cfcorp_web_instance" {
   ami           = "${var.ami_id}"
   instance_type = "${var.instance_type}"
   key_name = "${var.key_name}"
   security_groups = [
-        "${aws_security_group.cfcorp.name}"
+        "${aws_security_group.cfcorp_web_sg.name}"
   ]
   user_data = "${file("userdata.sh")}"
 
   tags {
     Name = "${var.tag_name}"
+  }
+}
+
+
+
+resource "aws_elb" "cfcorp_web_elb" {
+  name               = "cfcorp-web-elb"
+  availability_zones = ["${aws_instance.cfcorp_web_instance.availability_zone}"]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
+  instances                   = ["${aws_instance.cfcorp_web_instance.id}"]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags {
+    Name = "foobar-terraform-elb"
   }
 }
